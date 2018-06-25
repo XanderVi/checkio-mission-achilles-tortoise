6d08bdf037d2293a585000e4f2250a37b36caa2f
XanderVi <engineermexanic@gmail.com>
2018-06-25 12:30:03 +0300

    fix animation bug

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,48 +1,121 @@
-requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
-    function (extIO, $, TableComponent) {
-        var io = new extIO({
-            functions: {
-                js: 'chase',
-                python: 'chase'
-            }
+//Dont change it
+requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
+    function (ext, $, Raphael, Snap) {
+
+        var cur_slide = {};
+
+        ext.set_start_game(function (this_e) {
         });
-        io.start();
+
+        ext.set_process_in(function (this_e, data) {
+            cur_slide = {};
+            cur_slide["in"] = data[0];
+            this_e.addAnimationSlide(cur_slide);
+        });
+
+        ext.set_process_out(function (this_e, data) {
+            cur_slide["out"] = data[0];
+        });
+
+        ext.set_process_ext(function (this_e, data) {
+            cur_slide.ext = data;
+        });
+
+        ext.set_process_err(function (this_e, data) {
+            cur_slide['error'] = data[0];
+            this_e.addAnimationSlide(cur_slide);
+            cur_slide = {};
+        });
+
+        ext.set_animate_success_slide(function (this_e, options) {
+            var $h = $(this_e.setHtmlSlide('<div class="animation-success"><div></div></div>'));
+            this_e.setAnimationHeight(115);
+        });
+
+        ext.set_animate_slide(function (this_e, data, options) {
+            var $content = $(this_e.setHtmlSlide(ext.get_template('animation'))).find('.animation-content');
+            if (!data) {
+                console.log("data is undefined");
+                return false;
+            }
+
+            //YOUR FUNCTION NAME
+            var fname = 'chase';
+
+            var checkioInput = data.in || [6, 3, 2];
+            var checkioInputStr = fname + '(' + checkioInput.join(", ")  + ')';
+
+            var failError = function (dError) {
+                $content.find('.call').html(checkioInputStr);
+                $content.find('.output').html(dError.replace(/\n/g, ","));
+
+                $content.find('.call').addClass('error');
+		$content.find('.output').addClass('error');
+                
+                $content.find('.answer').remove();
+                $content.find('.explanation').remove();
+                this_e.setAnimationHeight($content.height() + 60);
+            };
+
+            if (data.error) {
+                failError(data.error);
+                return false;
+            }
+
+            if (data.ext && data.ext.inspector_fail) {
+                failError(data.ext.inspector_result_addon);
+                return false;
+            }
+
+            $content.find('.call').html(checkioInputStr);
+            $content.find('.output').html('Working...');
+
+
+            if (data.ext) {
+                var rightResult = data.ext["answer"];
+                var userResult = data.out;
+                var result = data.ext["result"];
+                var result_addon = data.ext["result_addon"];
+
+                //if you need additional info from tests (if exists)
+                var explanation = data.ext["explanation"];
+                $content.find('.output').html('&nbsp;Your result:&nbsp;' + JSON.stringify(userResult));
+                if (!result) {
+                    $content.find('.call').addClass('error');
+		    $content.find('.output').addClass('error');
+		    $content.find('.answer').html('Right result:&nbsp;' + JSON.stringify(rightResult));
+                    $content.find('.answer').addClass('error');
+                    
+                    
+                }
+                else {
+                    $content.find('.answer').remove();
+                }
+            }
+            else {
+                $content.find('.answer').remove();
+            }
+
+            this_e.setAnimationHeight($content.height() + 60);
+
+        });
+
+        var colorOrange4 = "#F0801A";
+        var colorOrange3 = "#FA8F00";
+        var colorOrange2 = "#FAA600";
+        var colorOrange1 = "#FABA00";
+
+        var colorBlue4 = "#294270";
+        var colorBlue3 = "#006CA9";
+        var colorBlue2 = "#65A1CF";
+        var colorBlue1 = "#8FC7ED";
+
+        var colorGrey4 = "#737370";
+        var colorGrey3 = "#9D9E9E";
+        var colorGrey2 = "#C5C6C6";
+        var colorGrey1 = "#EBEDED";
+
+        var colorWhite = "#FFFFFF";
+
     }
 );
-
-multipleArguments: true,
-
-animationTemplateName: 'animation',
-
-animation: function($expl, data){
-                if (!data.ext || !data.ext.explanation) {
-                    return;
-                }
-                var expl = data.ext.explanation;
-                $expl.addClass('output').html(expl);
-            },
-
-animation: function($expl, data){
-                var checkioInput = data.in;
-                if (!checkioInput){
-                    return;
-                }
-                var canvas = new MedianCanvas($expl[0], checkioInput);
-                canvas.createCanvas();
-                canvas.animateCanvas();
-            },
-
-,
-            retConsole: function (ret) {
-                $tryit.find(".checkio-result").html("Your Result
-" + ret);
-            }
-
-,
-            tryit:function (this_e) {
-
-}
-
-$tryit = $(this_e.extSetHtmlTryIt(this_e.getTemplate('tryit')));
-this_e.extSendToConsoleCheckiO(tCanvas.gatherData());
-
